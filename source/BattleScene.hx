e36c07746ce0ea306e9e24dc06d20a685b8b24cd
riley <rileywestonmiller@outlook.com>
2019-12-04 22:21:16 -0800

    started battle scene

--- source/BattleScene.hx
+++ source/BattleScene.hx
@@ -1,12 +1,27 @@
 package;
 
+import flixel.util.FlxSave;
 import flixel.FlxState;
 
 class BattleScene extends FlxState
 {
+	var gameData:GameData;
+	var gameSave:FlxSave;
+	var characters:Array<Character>;
+	
 	override public function create():Void
 	{
 		super.create();
+		gameData = new GameData();
+		gameSave = new FlxSave();
+		gameSave.bind("global");
+		characters = new Array<Character>();
+		for (i in 0...1){
+			var char = new Character();
+			char.randomize();
+			add(char);
+			characters.push(char);
+		}
 	}
 
 	override public function update(elapsed:Float):Void
--- source/Character.hx
+++ source/Character.hx
@@ -1,12 +1,13 @@
 package;
 
+import flixel.util.FlxColor;
+import flixel.group.FlxGroup;
+import haxe.DynamicAccess;
 import flixel.FlxSprite;
 
-class Character extends FlxSprite{
-    var hatInd:Int = 0;
-    var headInd:Int = 0;
-    var topInd:Int = 0;
-    var bottomInd:Int = 0;
+class Character extends FlxGroup{
+    var x:Float = 0;
+    var y:Float = 0;
     var hatSprite:FlxSprite;
     var headSprite:FlxSprite;
     var topSprite:FlxSprite;
@@ -20,22 +21,59 @@ class Character extends FlxSprite{
     var weaknesses:Array<String>;
     var tempWeaknesses:Array<{name:String, duration:Float}>;
     var buffs:Array<{name:String, duration:Float, mod:Float}>;
-    function new(x:Float, y:Float, data:Dynamic) {
-        super(x,y);
+    public function new(x:Float = 0, y:Float = 0) {
+        super();
+        this.x = x;
+        this.y = y;
         hatSprite = new FlxSprite();
-        hatSprite.loadGraphic("assets/Hats.png", true, 64,64);
+        add(hatSprite);
+        hatSprite.makeGraphic(50,100,FlxColor.GREEN);
+        /*hatSprite.loadGraphic("assets/Hats.png", true, 64,64);
         headSprite = new FlxSprite();
+        add(headSprite);
         headSprite.loadGraphic("assets/Heads.png", true, 64,64);
         topSprite = new FlxSprite();
+        add(topSprite);
         topSprite.loadGraphic("assets/Tops.png", true, 64,64);
         bottomSprite = new FlxSprite();
-        bottomSprite.loadGraphic("assets/Bottoms.png", true, 64,64);
+        add(bottomSprite);
+        bottomSprite.loadGraphic("assets/Bottoms.png", true, 64,64);*/
+    }
+    override public function update(elapsed:Float) {
+        hatSprite.x = x;
+        hatSprite.y = y;
+        super.update(elapsed);
+    }
+    public function loadData(data:Dynamic){
+        sass = data.sass;
+        wit = data.wit;
+        moxie = data.moxie;
+        flair = data.flair;
+        realness = data.realness;
+        hatSprite.animation.frameIndex = data.hatInd;
+        headSprite.animation.frameIndex = data.headInd;
+        topSprite.animation.frameIndex = data.topInd;
+        bottomSprite.animation.frameIndex = data.bottomInd;
+    }
+    public function saveData(){
+        var data:Dynamic = {
+            sass : this.sass,
+            wit : this.wit,
+            moxie : this.moxie,
+            flair : this.flair,
+            realness : this.realness,
+            hatInd : this.hatSprite.animation.frameIndex,
+            headInd : this.headSprite.animation.frameIndex,
+            topInd : this.topSprite.animation.frameIndex,
+            bottomInd : this.bottomSprite.animation.frameIndex
+        }
+        return data;
     }
     function randint(min:Int, max:Int){
         var num = Math.random() * (max - min);
         return Math.round(num) + min;
     }
-    function randomize(){
+    public function randomize(){
         var stats = new Array<Float>();
         var num:Float;
         while(stats.length < 2){
@@ -57,9 +95,9 @@ class Character extends FlxSprite{
         flair = stats[3];
         realness = stats[4];
         quiddity = stats[5];
-        hatInd = randint(0,hatSprite.animation.frames-1);
-        headInd = randint(0,headSprite.animation.frames-1);
-        topInd = randint(0,topSprite.animation.frames-1);
-        bottomInd = randint(0,bottomSprite.animation.frames-1);
+        hatSprite.animation.frameIndex = randint(0,hatSprite.animation.frames-1);
+        /*headSprite.animation.frameIndex = randint(0,headSprite.animation.frames-1);
+        topSprite.animation.frameIndex = randint(0,topSprite.animation.frames-1);
+        bottomSprite.animation.frameIndex = randint(0,bottomSprite.animation.frames-1);*/
     }
 }
